142a51cfe51ba1bb881fa4243b3cd4a48ca17d1c
techvaVinz <techvassist@gmail.com>
2026-03-06 08:06:51 +0800

    Update docker-entrypoint.sh

--- docker/images/n8n/docker-entrypoint.sh
+++ docker/images/n8n/docker-entrypoint.sh
@@ -1,15 +1,3 @@
-#!/bin/sh
-if [ -d /opt/custom-certificates ]; then
-  echo "Trusting custom certificates from /opt/custom-certificates."
-  export NODE_OPTIONS="--use-openssl-ca $NODE_OPTIONS"
-  export SSL_CERT_DIR=/opt/custom-certificates
-  c_rehash /opt/custom-certificates
-fi
-
-if [ "$#" -gt 0 ]; then
-  # Got started with arguments
-  exec n8n "$@"
-else
-  # Got started without arguments
-  exec n8n
-fi
+# Replace <n8n-version-number> with the n8n release version number. 
+# For example, N8N_VERSION=0.177.0
+docker build --build-arg N8N_VERSION=<n8n-version-number> --tag=customizedn8n .
